64a36a145daa9255ec4057ffbc8c21ba95411a4d
gabrielcoder <gabrielcoder247@gmail.com>
2019-05-23 18:19:55 +0300

    deploying for testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBKzK34FDJfo4s0H3--48ctWRZJVbwyCW4",
+    authDomain: "fir-apps-accb4.firebaseapp.com",
+    databaseURL: "https://fir-apps-accb4.firebaseio.com",
+    projectId: "fir-apps-accb4",
+    storageBucket: "fir-apps-accb4.appspot.com",
+    messagingSenderId: "893753741120",
+    appId: "1:893753741120:web:cf6a707388765025"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: true,
-  // firebase: {
-  //   apiKey: "AIzaSyBKzK34FDJfo4s0H3--48ctWRZJVbwyCW4",
-  //   authDomain: "fir-apps-accb4.firebaseapp.com",
-  //   databaseURL: "https://fir-apps-accb4.firebaseio.com",
-  //   projectId: "fir-apps-accb4",
-  //   storageBucket: "fir-apps-accb4.appspot.com",
-  //   messagingSenderId: "893753741120",
-  //   appId: "1:893753741120:web:d6d68c3021432213"
-  // },
   firebase : {
     apiKey: "AIzaSyBKzK34FDJfo4s0H3--48ctWRZJVbwyCW4",
     authDomain: "fir-apps-accb4.firebaseapp.com",
